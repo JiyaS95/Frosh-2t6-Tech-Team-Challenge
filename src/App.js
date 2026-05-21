2376e9b3772bdb81fdd5e0d1b9328562b147355a
JiyaS95 <shukla.jiya95@outlook.com>
2026-05-21 22:25:51 +0000

    first commit: shows day names

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import scheduleData from './schedule_data.json';
 // TODO: Build your Frosh Week Schedule page here
 
 function App() {
+  const days = Object.keys(scheduleData);
+
   return (
     <div>
-      <h1>Good Luck, Applicant!</h1>
-      {/*feel free to remove this*/}
+      <h1>F!ROSH Week 2T6</h1>
+      {days.map(day => (
+        <h2 key={day}>{day}</h2>
+      ))}
     </div>
   );
 }
